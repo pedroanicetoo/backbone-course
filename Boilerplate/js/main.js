8173ade7b781b8c49331a279d7563c5b815b0c2b
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2020-03-17 13:43:05 -0300

    Some examples on model

--- Boilerplate/js/main.js
+++ Boilerplate/js/main.js
@@ -1,7 +1,47 @@
-var Song = Backbone.Model.extend({
-  initialize: function(){
-    console.log("A new song has been created");
-  }
-});
 
-var song = new Song();
+
+
+// INHERITANCE
+
+// var Animal = Backbone.Model.extend({
+// 	walk: function(){
+// 		console.log("Animal walking...");
+// 	}
+// });
+
+// var Dog = Animal.extend({
+// 	walk: function(){
+// 		Animal.prototype.walk.apply(this);
+// 		console.log("Dog walking...")
+// 	}
+// });
+
+// var dog = new Dog();
+
+// dog.walk();
+
+// var Song = Backbone.Model.extend({
+// 	defaults: {
+// 		genre: "Jazz"
+// 	}
+// });
+// var song = new Song({
+// 	title: "Blue in Green",
+// 	artist: "Miles Davis",
+// 	publishYear: 1959	
+// });
+
+// VALIDATION
+
+// var Song = Backbone.Model.extend({
+// 		validate: function(attrs) {
+// 			if(!attrs.title)
+// 					return "title is required";
+// 		}
+// });
+
+// var song = new Song();
+
+//song.isValid();
+//var lastError = song.validationError;
+
