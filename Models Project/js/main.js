2129168e92d3bc6d87380d7e85d4c217dec88a43
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2020-03-23 01:33:33 -0300

    Model practice example

---
+++ Models Project/js/main.js
@@ -0,0 +1,41 @@
+var Vehicle = Backbone.Model.extend({
+	
+	idAttribute: "registrationNumber",
+
+	urlRoot: "/api/vehicles",
+
+	validate: function(attrs){
+		if (!attrs.registrationNumber)
+			return "Vehicle is not valid.";
+	},
+
+	start: function() {
+		console.log("Vehicle started.");
+	}
+});
+
+var Car = Vehicle.extend({
+	start: function(){
+		console.log("Car with registration number " + this.get("registrationNumber") + " started.");
+	}
+});
+
+car = new Car({
+	registrationNumber: "XLI887",
+	color: "Blue"
+});
+
+car.unset("registrationNumber");
+
+if (!car.isValid())
+	console.log(car.validationError);
+
+car.set("registrationNumber", "XLI887");
+
+if (!car.isValid())
+	console.log(car.validationError);
+
+car.start();
+
+
+
