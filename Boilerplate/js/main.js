63e5740b8e72c22ec6aa90eb5e83314beb220059
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2020-03-23 02:05:45 -0300

    Some methods to manipulate models on collection

--- Boilerplate/js/main.js
+++ Boilerplate/js/main.js
@@ -1,22 +1,57 @@
 var Song = Backbone.Model.extend();
 
 var Songs = Backbone.Collection.extend({
-	Model: Song
+		model: Song
 });
 
-var songs = new Songs([
-		new Song({ title: "Song 1"}),
-		new Song({ title: "Song 2"}),
-		new Song({ title: "Song 3"})
-]);
+var songs = new Songs();
 
-songs.add(new Song({ title: "Song 4" }));
+songs.add(new Song({ title: "Song 1", genre: "Jazz", downloads: 110 }), { at: 0 });
 
-//get specific model by client id
-var firstSong = songs.at(0);
+songs.push(new Song({ title: "Song 2", genre: "Jazz", downloads: 90 }));
 
-//get specific model by cid
-var songWithIdC1 = songs.get("c1");
+var jazzSongs = songs.where({ genre: "Jazz" });
 
-//Remove specific model
-songs.remove(firstSong);
+var firstJazzSong = songs.findWhere({ genre: "Jazz" });
+
+console.log("jazzSongs", jazzSongs);
+
+console.log("First Jazz Song", firstJazzSong);
+
+var filteredSongs = songs.where({ genre: "Jazz", title: "Song 2" });
+console.log("Filtered Songs", filteredSongs);
+
+var topDownloads = songs.filter(function(song){
+		return song.get("downloads") > 100
+});
+
+console.log("Top Downloads", topDownloads);
+
+songs.each(function(song) {
+		console.log(song);
+});
+
+// var Song = Backbone.Model.extend();
+
+// var Songs = Backbone.Collection.extend({
+// 	Model: Song
+// });
+
+// var songs = new Songs([
+// 		new Song({ title: "Song 1"}),
+// 		new Song({ title: "Song 2"}),
+// 		new Song({ title: "Song 3"})
+// ]);
+
+// songs.add(new Song({ title: "Song 4" }));
+
+// //get specific model by client id
+// var firstSong = songs.at(0);
+
+// //get specific model by cid
+// var songWithIdC1 = songs.get("c1");
+
+// //Remove specific model
+// songs.remove(firstSong);
+
+
