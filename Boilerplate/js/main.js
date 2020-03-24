a1bfcc8b65fad7db1af77957845f6782b77f06fc
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2020-03-24 01:45:24 -0300

    Creating Views, introduction example

--- Boilerplate/js/main.js
+++ Boilerplate/js/main.js
@@ -1,57 +1,22 @@
-var Song = Backbone.Model.extend();
+var SongView = Backbone.View.extend({
 
-var Songs = Backbone.Collection.extend({
-		model: Song
+	tagName: "span",
+
+	className: "song",
+
+	id: "1234",
+
+	attributes: {
+		"data-genre": "Jazz"
+	},
+
+	render: function(){
+		this.$el.html("Hello World");
+
+		return this
+	}
 });
 
-var songs = new Songs();
+var songView = new SongView();
 
-songs.add(new Song({ title: "Song 1", genre: "Jazz", downloads: 110 }), { at: 0 });
-
-songs.push(new Song({ title: "Song 2", genre: "Jazz", downloads: 90 }));
-
-var jazzSongs = songs.where({ genre: "Jazz" });
-
-var firstJazzSong = songs.findWhere({ genre: "Jazz" });
-
-console.log("jazzSongs", jazzSongs);
-
-console.log("First Jazz Song", firstJazzSong);
-
-var filteredSongs = songs.where({ genre: "Jazz", title: "Song 2" });
-console.log("Filtered Songs", filteredSongs);
-
-var topDownloads = songs.filter(function(song){
-		return song.get("downloads") > 100
-});
-
-console.log("Top Downloads", topDownloads);
-
-songs.each(function(song) {
-		console.log(song);
-});
-
-// var Song = Backbone.Model.extend();
-
-// var Songs = Backbone.Collection.extend({
-// 	Model: Song
-// });
-
-// var songs = new Songs([
-// 		new Song({ title: "Song 1"}),
-// 		new Song({ title: "Song 2"}),
-// 		new Song({ title: "Song 3"})
-// ]);
-
-// songs.add(new Song({ title: "Song 4" }));
-
-// //get specific model by client id
-// var firstSong = songs.at(0);
-
-// //get specific model by cid
-// var songWithIdC1 = songs.get("c1");
-
-// //Remove specific model
-// songs.remove(firstSong);
-
-
+$("#container").html(songView.render().$el);
