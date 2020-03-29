11923079add8d74ee365143a26da37619a020bb8
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2020-03-29 19:35:30 -0300

    List collection on models on a view example

--- Boilerplate/js/main.js
+++ Boilerplate/js/main.js
@@ -1,22 +1,34 @@
+var Song = Backbone.Model.extend();
+
+var Songs = Backbone.Collection.extend({
+	model: Song
+});
+
 var SongView = Backbone.View.extend({
+	tagName: "li",
 
-	tagName: "span",
-
-	className: "song",
-
-	id: "1234",
-
-	attributes: {
-		"data-genre": "Jazz"
-	},
-
-	render: function(){
-		this.$el.html("Hello World");
-
-		return this
+	render: function() {
+			this.$el.html(this.model.get("title"));
+			
+			return this;
 	}
 });
 
-var songView = new SongView();
+var SongsView = Backbone.View.extend({
+	render: function() {
+		var self = this;
+		this.model.each(function(song) {
+			var songView = new SongView({ model: song });
+			self.$el.append(songView.render().$el);
+		});
+	}
+});
 
-$("#container").html(songView.render().$el);
+var songs = new Songs([
+	new Song({ title: "Blue and Green"}),
+	new Song({ title: "So what"}),
+	new Song({ title: "All Blues"})
+	]);
+
+var songsView = new SongsView({ el: "#songs", model: songs});
+songsView.render();
