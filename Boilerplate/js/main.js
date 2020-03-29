5a5d0b8dce9152876fc49f4e8c24b17276edfd8b
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2020-03-29 20:57:07 -0300

    Handling Collection Events

--- Boilerplate/js/main.js
+++ Boilerplate/js/main.js
@@ -1,47 +1,55 @@
-var Song = Backbone.Model.extend({
-	defaults: {
-		listeners: 0
-	}
+var Song = Backbone.Model.extend();
+
+var Songs = Backbone.Collection.extend({
+	model: Song
 });
 
-// var Songs = Backbone.Collection.extend({
-// 	model: Song
-// });
-
 var SongView = Backbone.View.extend({
-	initialize: function(){
-		this.model.on("change", this.onModelChange, this);
-	},
-
-	onModelChange: function(){ // action from the view for the onChange model 
-		this.$el.addClass("someClass");
-	},
+	
+	tagName: "li",
 
 	render: function() {
-			this.$el.html(this.model.get("title") + " - Listeners: "+ this.model.get("listeners"));
-			
+			this.$el.html(this.model.get("title"));		
+			this.$el.attr("id", this.model.id);
+
 			return this;
 	}
 });
 
-// var SongsView = Backbone.View.extend({
-// 	render: function() {
-// 		var self = this;
-// 		this.model.each(function(song) {
-// 			var songView = new SongView({ model: song });
-// 			self.$el.append(songView.render().$el);
-// 		});
-// 	}
-// });
+var SongsView = Backbone.View.extend({
+	
+	tagName: "ul",
 
-// var songs = new Songs([
-// 	new Song({ title: "Blue and Green"}),
-// 	new Song({ title: "So what"}),
-// 	new Song({ title: "All Blues"})
-// 	]);
+	initialize: function(){
+		this.model.on("add", this.onSongAdded, this);
+		this.model.on("remove", this.onSongRemoved, this);
+	},
 
-var song = new Song({ title: "Blue and Green" });
+	onSongAdded: function(song){
+		var songView = new SongView({ model: song });
 
-var songView = new SongView({ el: "#container", model: song });
+		this.$el.append(songView.render().$el);
+	},
 
-songView.render();
+	onSongRemoved: function(song){
+		// this.$el.find("li#" + song.id).remove();
+		this.$("li#" + song.id).remove();
+	},
+
+	render: function() {
+		var self = this;
+		this.model.each(function(song) {
+			var songView = new SongView({ model: song });
+			self.$el.append(songView.render().$el);
+		});
+	}
+});
+
+var songs = new Songs([
+	new Song({ id: 1, title: "Blue and Green"}),
+	new Song({ id: 2, title: "So what"}),
+	new Song({ id: 3, title: "All Blues"})
+	]);
+
+var songsView = new SongsView({ el: "#songs", model: songs});
+songsView.render();
