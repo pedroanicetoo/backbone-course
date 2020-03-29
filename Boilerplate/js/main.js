58997fc0dd17d4aa335c0321f51f5eb632d62575
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2020-03-29 20:08:48 -0300

    Use events on a view

--- Boilerplate/js/main.js
+++ Boilerplate/js/main.js
@@ -1,34 +1,48 @@
 var Song = Backbone.Model.extend();
 
-var Songs = Backbone.Collection.extend({
-	model: Song
-});
+// var Songs = Backbone.Collection.extend({
+// 	model: Song
+// });
 
 var SongView = Backbone.View.extend({
-	tagName: "li",
+	events: {
+		"click": "onClick",
+		"click .bookmark": "onClickBookmark",
+
+	},
+	onClick: function(){
+		console.log("Listen Clicked");
+	},
+	onClickBookmark: function(e){
+		e.stopPropagation();
+		console.log('Bookmark clicked')
+	},
 
 	render: function() {
-			this.$el.html(this.model.get("title"));
+			this.$el.html(this.model.get("title") + " <button>Listen </button> <button class='bookmark'>Bookmark</button>");
 			
 			return this;
 	}
 });
 
-var SongsView = Backbone.View.extend({
-	render: function() {
-		var self = this;
-		this.model.each(function(song) {
-			var songView = new SongView({ model: song });
-			self.$el.append(songView.render().$el);
-		});
-	}
-});
+// var SongsView = Backbone.View.extend({
+// 	render: function() {
+// 		var self = this;
+// 		this.model.each(function(song) {
+// 			var songView = new SongView({ model: song });
+// 			self.$el.append(songView.render().$el);
+// 		});
+// 	}
+// });
 
-var songs = new Songs([
-	new Song({ title: "Blue and Green"}),
-	new Song({ title: "So what"}),
-	new Song({ title: "All Blues"})
-	]);
+// var songs = new Songs([
+// 	new Song({ title: "Blue and Green"}),
+// 	new Song({ title: "So what"}),
+// 	new Song({ title: "All Blues"})
+// 	]);
 
-var songsView = new SongsView({ el: "#songs", model: songs});
+var song = new Song({ title: "Blue and Green" });
+
+var songsView = new SongView({ el: "#container", model: song });
+
 songsView.render();
