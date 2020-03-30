a29e83b4ff4987a37fbd4bb7d65cbd45f6dc0e5f
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2020-03-29 21:08:38 -0300

    Templating example

--- Boilerplate/js/main.js
+++ Boilerplate/js/main.js
@@ -1,55 +1,16 @@
 var Song = Backbone.Model.extend();
 
-var Songs = Backbone.Collection.extend({
-	model: Song
-});
-
 var SongView = Backbone.View.extend({
-	
-	tagName: "li",
-
-	render: function() {
-			this.$el.html(this.model.get("title"));		
-			this.$el.attr("id", this.model.id);
+		render: function(){
+			var template = _.template($("#songTemplate").html());
+			var html = template(this.model.toJSON());
+			this.$el.html(html);
 
 			return this;
-	}
+		}
 });
 
-var SongsView = Backbone.View.extend({
-	
-	tagName: "ul",
+var song = new Song({ title: "Blue and Green", plays: 1001 });
 
-	initialize: function(){
-		this.model.on("add", this.onSongAdded, this);
-		this.model.on("remove", this.onSongRemoved, this);
-	},
-
-	onSongAdded: function(song){
-		var songView = new SongView({ model: song });
-
-		this.$el.append(songView.render().$el);
-	},
-
-	onSongRemoved: function(song){
-		// this.$el.find("li#" + song.id).remove();
-		this.$("li#" + song.id).remove();
-	},
-
-	render: function() {
-		var self = this;
-		this.model.each(function(song) {
-			var songView = new SongView({ model: song });
-			self.$el.append(songView.render().$el);
-		});
-	}
-});
-
-var songs = new Songs([
-	new Song({ id: 1, title: "Blue and Green"}),
-	new Song({ id: 2, title: "So what"}),
-	new Song({ id: 3, title: "All Blues"})
-	]);
-
-var songsView = new SongsView({ el: "#songs", model: songs});
-songsView.render();
+var songView = new SongView({ el: "#container", model: song });
+songView.render();
